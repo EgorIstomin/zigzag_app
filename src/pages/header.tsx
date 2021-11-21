f15ee57a706bc58d8f45208654bff2ad125fc9d7
Egor I <istomin@zoho.eu>
2021-11-21 14:05:54 -0500

    Added some changes

---
+++ src/pages/header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const Header: React.FunctionComponent = () => ( 
+    <div className='wrapper'>
+        <div className='container'>
+            <div className="logo">
+                <h1>zigzag</h1>
+            </div>
+            <div className="menu">
+                <div>
+                    <a>Premium</a>
+                    <a>Упражнения</a>
+                    <a>Советы</a>
+                    <a>Школы вождения</a>
+                    <a>Журнал</a>
+                
+                </div>
+            </div>
+
+    </div>
+    </div>
+)
